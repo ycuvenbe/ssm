c37e0db6190c500a298cb0cea05a211706d5f6cc
ycuvenbe <daos7552958@163.com>
2021-05-11 21:38:08 +0800

    weda

--- src/main/java/com/ssm/controller/CaptchaController.java
+++ src/main/java/com/ssm/controller/CaptchaController.java
@@ -37,7 +37,7 @@ public class CaptchaController {
         response.setDateHeader("Expires", 0);
         response.setHeader("P3P","CP=CAO PSA OUR");
         // Set standard HTTP/1.1 no-cache headers.
-        response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+//        response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
 
         // Set IE extended HTTP/1.1 no-cache headers (use addHeader).
         response.addHeader("Cache-Control", "post-check=0, pre-check=0");
